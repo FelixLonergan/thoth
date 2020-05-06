4fd489c129dffce76f486984822e4c05fc342ade
Felix <felix.lonergan@gmail.com>
2020-05-06 19:10:08 +1000

    Add option to hide article text

--- thoth/__main__.py
+++ thoth/__main__.py
@@ -5,12 +5,14 @@ from thoth.helper import get_handler
 article = st.sidebar.selectbox(
     "Choose a Machine Learning method", ["Decision Trees"], index=0,
 )
-
+show_text = st.sidebar.checkbox("Show article text", value=True)
 st.title(article)
 handler = get_handler(article)
-st.write(handler.get_section("intro"))
-st.altair_chart(handler.get_summary(), use_container_width=True)
 
+if show_text:
+    st.altair_chart(handler.get_summary(), use_container_width=True)
+    st.write(handler.get_section("intro"), unsafe_allow_html=True)
+    st.write("---")
 handler.render_eda()
 
 handler.render_playground()

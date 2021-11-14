14858313b2fa9707264fad3c285684ad5bb03ecd
Felix Lonergan <felix.lonergan@gmail.com>
2021-11-14 22:42:46 +1100

    Migrate to CLI to run

--- thoth/__init__.py
+++ thoth/__init__.py
@@ -1,3 +1,10 @@
-__version__ = "0.1.0"
+import pkg_resources as _pkg_resources
+
+try:
+    _dist = _pkg_resources.get_distribution("thoth")
+except _pkg_resources.DistributionNotFound:
+    __version__ = None
+else:
+    __version__ = _dist.version
 
 SEED = 42
--- thoth/__main__.py
+++ thoth/__main__.py
@@ -2,17 +2,29 @@ import streamlit as st
 
 from thoth.helper import get_handler
 
-article = st.sidebar.selectbox(
-    "Choose a Machine Learning method", ["Decision Trees"], index=0,
-)
-show_text = st.sidebar.checkbox("Show article text", value=True)
-st.title(article)
-handler = get_handler(article)
+ARTICLES = ["Decision Trees"]
 
-if show_text:
+
+def main():
+    """Main entry point for the Thoth application that handles overall page structure"""
+    st.sidebar.title("Thoth")
+    article = st.sidebar.selectbox(
+        "Choose a Machine Learning method",
+        ARTICLES,
+        index=0,
+    )
+
+    st.title(article)
+    handler = get_handler(article)
+
     st.altair_chart(handler.get_summary(), use_container_width=True)
     st.write(handler.get_section("intro"), unsafe_allow_html=True)
-    st.write("---")
-handler.render_eda()
 
-handler.render_playground()
+    st.write("---")
+
+    handler.render_eda()
+    handler.render_playground()
+
+
+if __name__ == "__main__":
+    main()
